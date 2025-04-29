af8451bad4f48a62e4d3c93fcd60b0af240c90cd
Rishab Sharma <rishabhsharma88996@gmail.com>
2025-04-29 14:42:45 +0530

    updated HTML & JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Graphic Designer"],
+    strings: ["Web Developer", "Java Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
